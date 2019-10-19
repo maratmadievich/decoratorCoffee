cfd445a045fe61d1cf2e4c3f294c429157b16b5f
Marat <iknow.whoiam@mail.ru>
2019-10-19 22:00:37 +0300

    Initial Commit

---
+++ Coffee Maker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coffee Maker
+//
+//  Created by Mac on 19.10.2019.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
